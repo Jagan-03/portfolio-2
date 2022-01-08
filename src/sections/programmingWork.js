5fbcdfbaff66575ec1274e8e83d70bd6e5704cb5
Jaganath Ezhilarasu <jaganmj324@gmail.com>
2022-01-08 10:22:32 +0530

    Work link added

--- src/sections/programmingWork.js
+++ src/sections/programmingWork.js
@@ -7,8 +7,10 @@ const ProgrammingWork = () => {
         <h1 className="text-light">Competitive Programming Work</h1>
       </div>
         <hr />
-        <div className="work-box" href="https://www.guvi.in/jaganmj324" target="_blank" rel="noreferrer" >
+        <div className="work-box">
+        <a href="https://www.guvi.in/jaganmj324" target="_blank" rel="noreferrer" >
          <img width={200} className="cp-logo" src="https://s3.ap-south-1.amazonaws.com/guvi-2.0/rewards-logo/codekata.png" alt="logo" />
+        </a>
         </div>
     </div>
   );
